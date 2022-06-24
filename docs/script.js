9ae4331725098b1aa2ec1e33499614a9fa4f74cf
zosiunia <103433637+zosiunia@users.noreply.github.com>
2022-06-24 12:08:44 +0200

    dodnie nowego jsona, wyrównanie tekstu pod kamerkami w bootstrpie

--- docs/script.js
+++ docs/script.js
@@ -1,43 +1,94 @@
 function buttonpress(x){
 
-    console.log(x);
-  if (x=='a'){
-     document.getElementById("div1a").style.display ="block";
-     document.getElementById("div1b").style.display ="block";
-     document.getElementById("div1c").style.display ="block";
-  document.getElementById("div2").style.display ="none";
-  document.getElementById("div3").style.display ="none";
-    // document.getElementById("div1").style.zIndex="3";
-    //   document.getElementById("div2").style.zIndex="-1";
-    //   document.getElementById("div3").style.zIndex="-1";
-    }
-      
-    
-  else if(x=='b'){
-     document.getElementById("div1a").style.display ="none";
-     document.getElementById("div1b").style.display ="none";
-     document.getElementById("div1c").style.display ="none";
-  document.getElementById("div2").style.display ="block";
-  document.getElementById("div3").style.display ="none";
-    // document.getElementById("div1").style.zIndex="-1";
-      // document.getElementById("div2").style.zIndex="3";
-      // document.getElementById("div3").style.zIndex="-1";
-    }
-  else if(x=="c"){
-     document.getElementById("div1a").style.display ="none";
-     document.getElementById("div1b").style.display ="none";
-     document.getElementById("div1c").style.display ="none";
-  document.getElementById("div2").style.display ="none";
-  document.getElementById("div3").style.display ="block";
-    // document.getElementById("div1").style.zIndex="-1";
-    //   document.getElementById("div2").style.zIndex="-1";
-    //   document.getElementById("div3").style.zIndex="3";
-    }
+  console.log(x);
+if (x=='a'){
+   document.getElementById("div1a").style.display ="block";
+   document.getElementById("div1b").style.display ="block";
+   document.getElementById("div1c").style.display ="block";
+document.getElementById("div2a").style.display ="none";
+ document.getElementById("div2b").style.display ="none";
+  document.getElementById("div2c").style.display ="none";
+  document.getElementById("div2d").style.display ="none";
+  document.getElementById("div2e").style.display ="none";
+  document.getElementById("div2f").style.display ="none"; 
+document.getElementById("div3a").style.display ="none";
+  document.getElementById("div3b").style.display ="none";
+  document.getElementById("div3c").style.display ="none";
+  
+ 
   }
-  
-  //myclick();
-  //buttonpress("a");
-  
-  
-  buttonpress('a')
     
+  
+else if(x=='b'){
+   document.getElementById("div1a").style.display ="none";
+   document.getElementById("div1b").style.display ="none";
+   document.getElementById("div1c").style.display ="none";
+document.getElementById("div2a").style.display ="block";
+ document.getElementById("div2b").style.display ="block";
+  document.getElementById("div2c").style.display ="block";
+  document.getElementById("div2d").style.display ="block";
+  document.getElementById("div2e").style.display ="block";
+  document.getElementById("div2f").style.display ="block";
+document.getElementById("div3a").style.display ="none";
+  document.getElementById("div3b").style.display ="none";
+  document.getElementById("div3c").style.display ="none";
+  document.getElementById("div3d").style.display ="none";
+  }
+else if(x=="c"){
+   document.getElementById("div1a").style.display ="none";
+   document.getElementById("div1b").style.display ="none";
+   document.getElementById("div1c").style.display ="none";
+document.getElementById("div2a").style.display ="none";
+ document.getElementById("div2b").style.display ="none";
+  document.getElementById("div2c").style.display ="none";
+  document.getElementById("div2d").style.display ="none";
+  document.getElementById("div2e").style.display ="none";
+  document.getElementById("div2f").style.display ="none";
+document.getElementById("div3a").style.display ="block";
+  document.getElementById("div3b").style.display ="block";
+  document.getElementById("div3c").style.display ="block";
+  document.getElementById("div4a").style.display ="none";
+  document.getElementById("div4b").style.display ="none";
+  
+  }
+  else if(x=='b'){
+   document.getElementById("div1a").style.display ="none";
+   document.getElementById("div1b").style.display ="none";
+   document.getElementById("div1c").style.display ="none";
+document.getElementById("div2a").style.display ="block";
+ document.getElementById("div2b").style.display ="block";
+  document.getElementById("div2c").style.display ="block";
+  document.getElementById("div2d").style.display ="block";
+  document.getElementById("div2e").style.display ="block";
+  document.getElementById("div2f").style.display ="block";
+document.getElementById("div3a").style.display ="none";
+  document.getElementById("div3b").style.display ="none";
+  document.getElementById("div3c").style.display ="none";
+  document.getElementById("div3d").style.display ="none";
+    document.getElementById("div4a").style.display ="none";
+  document.getElementById("div4b").style.display ="none";
+  }
+else if(x=="d"){
+   document.getElementById("div1a").style.display ="none";
+   document.getElementById("div1b").style.display ="none";
+   document.getElementById("div1c").style.display ="none";
+document.getElementById("div2a").style.display ="none";
+ document.getElementById("div2b").style.display ="none";
+  document.getElementById("div2c").style.display ="none";
+  document.getElementById("div2d").style.display ="none";
+  document.getElementById("div2e").style.display ="none";
+  document.getElementById("div2f").style.display ="none";
+document.getElementById("div3a").style.display ="none";
+  document.getElementById("div3b").style.display ="none";
+  document.getElementById("div3c").style.display ="none";
+  document.getElementById("div4a").style.display ="block";
+  document.getElementById("div4b").style.display ="block";
+  }
+}
+
+
+
+buttonpress('a')
+buttonpress('b')
+buttonpress('c')
+buttonpress('d')
