711ab58aa3455cacee54f45e396660d3f354c6bd
zosiunia <103433637+zosiunia@users.noreply.github.com>
2022-06-28 14:13:40 +0200

    dodanie hamburger z ikonkami z figmy, poprawienie tła z naiskami,plus usunięcie konturu, problem z czerwona seksją

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,11 @@
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 function buttonpress(x){
 
   console.log(x);
